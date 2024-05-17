bccd7946abe9ce3ff1c3a78c8e5a4b5938138663
PuttipongMax <maxnetoogo@gmail.com>
2024-05-17 21:55:03 +0700

    feat commit and feature darkmode

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+import Center from './components/Center';
+
+function App() {
+
+  return (
+    <div className='text-xl'>
+     
+      {/* Header Section */}
+      <Header />
+
+      {/* Center Section */}
+      <Center />
+
+    </div>
+  )
+}
+
+export default App
